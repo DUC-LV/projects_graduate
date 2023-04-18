53a18f85830de70625463ba87a3e304830230f69
DucLV08 <duc080999@gmail.com>
2023-04-18 22:14:56 +0700

    api refresh token

--- backend/src/src/urls.py
+++ backend/src/src/urls.py
@@ -23,4 +23,5 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/login/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
