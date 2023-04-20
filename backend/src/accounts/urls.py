5d259d2283d8370274459c29f7348dbb02019570
DucLV08 <duc080999@gmail.com>
2023-04-20 22:43:37 +0700

    config urls

--- backend/src/accounts/urls.py
+++ backend/src/accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/register/', views.RegisterAPIView.as_view()),
-    path('user-info/', views.UserInfoAPIView.as_view()),
+    path('api/register', views.RegisterAPIView.as_view()),
+    path('user-info', views.UserInfoAPIView.as_view()),
 ]
