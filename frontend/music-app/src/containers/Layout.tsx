84102ce5245617db448e8d8583605a8f44991aab
DucLV08 <duc080999@gmail.com>
2023-04-29 23:12:48 +0700

    create radiopage and config scroll

--- frontend/music-app/src/containers/Layout.tsx
+++ frontend/music-app/src/containers/Layout.tsx
@@ -1,10 +1,18 @@
 import Header from "@/components/Header";
 import {Box, Grid} from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
 import { CurrentUserData } from "@/schemas";
+import { useRouter } from "next/router";
 
 const Layout = ({ children, currentUser }: React.PropsWithChildren<{ currentUser: CurrentUserData | null}>) => {
+	const router = useRouter();
+	const [overflow, setOverflow] = useState('auto');
+	useEffect(() => {
+		if(router.pathname === '/login' || router.pathname === '/register'){
+			setOverflow('hidden');
+		};
+	}, [router.pathname])
 	return(
 		<Grid container spacing={0} sx={{ backgroundColor: '#1d1d1d', width: '100vw', height: '100vh'}}>
 			<Grid item sx={{ maxWidth: '240px' }}>
@@ -14,7 +22,7 @@ const Layout = ({ children, currentUser }: React.PropsWithChildren<{ currentUser
 				<Grid item sx={{ maxHeight: '70px' }}>
 					<SearchBar currentUser={currentUser ? currentUser : null}/>
 				</Grid>
-				<Grid item container xs sx={{ overflow: 'auto'}}>
+				<Grid item container xs sx={{ overflow: `${overflow}`}}>
 					<Box sx={{ width: '100%', padding: '24px', height: 1000}}>
 						{children}
 					</Box>
---
+++ frontend/music-app/src/pages/radio/index.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const RadioPage = () => {
+	return(
+		<Box></Box>
+	);
+}
+
+export default RadioPage;
