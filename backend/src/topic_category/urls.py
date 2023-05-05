12c93e1ef648bb1c620cea35abcf4cf1727ac607
DucLV08 <duc080999@gmail.com>
2023-05-05 19:00:24 +0700

    api topic_category

--- backend/src/topic_category/urls.py
+++ backend/src/topic_category/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('update/topic-hub', views.TopicHubAPIView.as_view()),
     path('update/hub', views.HubAPIView.as_view()),
+    path('public/v1/composite/hub', views.HubPageAPIView.as_view()),
 ]
--- backend/src/topic_category/views.py
+++ backend/src/topic_category/views.py
@@ -1,10 +1,12 @@
+import random
+
 from django.shortcuts import render
 from rest_framework.views import APIView
 from django.http import HttpResponse
 from rest_framework.response import Response
 from .models import TopicHub, Hub, HubOfTopic
 from .serializers import TopicHubSerializers, HubSerializers
-from numpy import random
+from random import choice
 from rest_framework.permissions import AllowAny
 
 
@@ -64,3 +66,91 @@ class HubAPIView(APIView):
 
         serializer = HubSerializers(hubs, many=True)
         return Response(serializer.data)
+
+
+class HubPageAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        items = []
+
+        # banner
+        hub = Hub.objects.all()
+        banner = []
+
+        for hub in hub:
+            banner.append(HubSerializers(hub).data)
+
+        banner_data = random.choice(banner)
+
+        res_banner = {
+            "sectionType": "banner",
+            "viewType": "",
+            "title": "",
+            "link": "",
+            "items": banner_data
+        }
+
+        items.append(res_banner)
+
+        # hub highlight
+        topic_highlight = TopicHub.objects.filter(title="Nổi bật")
+        hub_topicHighlight = HubOfTopic.objects.filter(topic=topic_highlight[0])
+        data_hub_highlight = []
+
+        for hub_topic in hub_topicHighlight:
+            data_hub_highlight.append(HubSerializers(hub_topic.hub).data)
+
+        res_highlight = {
+            "sectionType": "highlight",
+            "viewType": "",
+            "title": "",
+            "link": "",
+            "items": data_hub_highlight
+        }
+
+        items.append(res_highlight)
+
+        # nations
+        topic_nation = TopicHub.objects.filter(title="Quốc gia")
+        hub_topicNation = HubOfTopic.objects.filter(topic=topic_nation[0])
+        data_hub_nations = []
+        for hub_topic in hub_topicNation:
+            data_hub_nations.append(HubSerializers(hub_topic.hub).data)
+
+        res_nations = {
+            "sectionType": "nation",
+            "viewType": "",
+            "title": "",
+            "link": "",
+            "items": data_hub_nations
+        }
+
+        items.append(res_nations)
+
+        # topic thịnh hành
+        topic_popular = TopicHub.objects.filter(title="Tâm trạng và hoạt động")
+        hub_topicPopular = HubOfTopic.objects.filter(topic=topic_popular[0])
+        data_hub_popular = []
+
+        for hub_topic in hub_topicPopular:
+            data_hub_popular.append(HubSerializers(hub_topic.hub).data)
+
+        res_popular = {
+            "sectionType": "popular",
+            "viewType": "",
+            "title": "",
+            "link": "",
+            "items": data_hub_popular
+        }
+
+        items.append(res_popular)
+
+        # respone
+        res = {
+            "err": 0,
+            "msg": "Success",
+            "data": items
+        }
+
+        return Response(res)
