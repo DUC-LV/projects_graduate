0e02e287ef26024c8b1b30d28ba8f360fc872845
DucLV08 <duc080999@gmail.com>
2023-05-09 22:45:15 +0700

    api playlist detail

--- backend/src/playlists/urls.py
+++ backend/src/playlists/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('update/topic-playlist', views.TopicPlaylistAPIView.as_view()),
     path('update/playlist', views.PlaylistAPIView.as_view()),
     path('update/playlist-data-sort', views.PlaylistDataSortAPIView.as_view()),
+    path('playlist/<str:id>', views.GetPlaylistDetailAPIView.as_view()),
 ]
--- backend/src/playlists/views.py
+++ backend/src/playlists/views.py
@@ -4,6 +4,11 @@ from rest_framework.response import Response
 from django.http import HttpResponse
 from .models import TopicPlaylist, Playlists, PlaylistOfTopic
 from .serializers import TopicPlaylistSerializers, PlaylistSerializers, PlaylistSortDataSerializers
+from artists.models import ArtistOfPlaylist, ArtistOfAlbum, ArtistOfSong
+from artists.serializers import ArtistSerializers
+from songs.models import SongOfPlaylist, SongOfAlbum
+from songs.serializers import SongSerializers
+from albums.serializers import AlbumSerializers
 from rest_framework.permissions import AllowAny
 
 
@@ -84,3 +89,70 @@ class PlaylistDataSortAPIView(APIView):
         serializers = PlaylistSortDataSerializers(playlist).data
 
         return Response(serializers)
+
+
+class GetPlaylistDetailAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request, id):
+
+        playlist = Playlists.objects.filter(id=id)
+        if not playlist.exists():
+            return HttpResponse(status=404)
+
+        artist_data = []
+        playlist_artist = ArtistOfPlaylist.objects.filter(playlist_id=playlist[0])
+
+        for artist in playlist_artist:
+            artist_data.append(ArtistSerializers(artist.artist).data)
+
+        playlist_data = PlaylistSerializers(playlist[0]).data
+        dict_artist_playlist = {"artists": artist_data}
+
+        # songs
+        song_data = []
+        song_playlist = SongOfPlaylist.objects.filter(playlist_id=playlist[0])
+
+        for song in song_playlist:
+
+            # artist
+            artist_song = ArtistOfSong.objects.filter(song_id=song.song)
+            artist_song_data = []
+
+            for artist_song in artist_song:
+                artist_song_data.append(ArtistSerializers(artist_song.artist).data)
+
+            # album
+            album_song = SongOfAlbum.objects.filter(song_id=song.song)
+            album_song_data = {}
+
+            for album_song in album_song:
+
+                artist_album = ArtistOfAlbum.objects.filter(album_id=album_song.album)
+                artist_album_data = []
+
+                # artist_album
+                for artist_album in artist_album:
+                    artist_album_data.append(ArtistSerializers(artist_album.artist).data)
+                album_song_data = dict(AlbumSerializers(album_song.album).data, **{"artist": artist_album_data})
+
+            # data_song
+            song_json = dict(SongSerializers(song.song).data, **{"artists": artist_song_data},
+                             **{"album": album_song_data})
+            song_data.append(song_json)
+
+        # playlist_detail
+        song_dict = {
+            "song": {
+                "items": song_data
+            }
+        }
+
+        playlist_dict = dict(playlist_data, **dict_artist_playlist, **song_dict)
+        res = {
+            "err": 0,
+            "msg": "Success",
+            "data": playlist_dict,
+        }
+
+        return Response(res)
