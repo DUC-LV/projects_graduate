b918c2bf97eb2c15863580c93e92fa0ba6095100
DucLV08 <duc080999@gmail.com>
2023-05-14 23:36:00 +0700

    ui topic-detail

--- frontend/music-app/src/pages/hub/[slugHub].tsx
+++ frontend/music-app/src/pages/hub/[slugHub].tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
+import { PlaylistSlider } from "@/components/home/Slide";
 import getTopicDetail from "@/services/getTopicDetail";
-import { Grid } from "@mui/material";
-import React from "react";
+import { Box } from "@mui/material";
+import React, { useCallback } from "react";
 
 type Props = {
 	data: any,
@@ -23,9 +25,42 @@ export async function getServerSideProps({ query }: any) {
 }
 
 const TopicDetail = ({ data }: Props) => {
-	console.log(data);
+
+	const SectionType = {
+		playlist: "playlist",
+	}
+
+	const generateContent = useCallback(() => {
+		return data?.sections?.map((section: any, index: number) => {
+
+			if(!section?.items || section?.items.length === 0){
+				return null;
+			}
+
+			switch(section?.sectionType){
+
+				case SectionType?.playlist:
+					return(
+						<PlaylistSlider
+							key={index}
+							data={section?.items}
+							title={section?.title}
+						/>
+					);
+
+				default:
+					return null;
+			}
+		})
+	}, [SectionType?.playlist, data?.sections])
+
 	return(
-		<Grid></Grid>
+		<Box>
+			<img alt="" src={data?.cover} style={{ height: '100%', width: '100%', borderRadius: '6px' }}/>
+			<Box sx={{ marginTop: '30px' }}>
+				{generateContent()}
+			</Box>
+		</Box>
 	);
 }
 
