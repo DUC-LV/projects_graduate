38d8dfbe16605ae7af9960f7cf53730f68c5f91b
DucLV08 <duc080999@gmail.com>
2023-05-26 23:08:13 +0700

    ui page mymusic

---
+++ frontend/music-app/src/components/playlist/ListPlaylist.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable @next/next/no-img-element */
+import { DataPlaylists } from "@/schemas";
+import { Grid, Typography } from "@mui/material";
+import React from "react";
+import { TextLineClamp } from "../Text";
+import { useRouter } from "next/router";
+import { convertSlug } from "@/untils";
+
+const ListPlaylist = (props: { data: Array<DataPlaylists>, title: string }) => {
+	const { data, title } = props;
+	const router = useRouter();
+	return(
+		<>
+			<Typography sx={{ fontSize: '1.5rem', fontWeight: '700', color: 'white', marginY: '15px' }}>{title}</Typography>
+			<Grid container spacing={2} columns={{ xs: 4, sm: 8, md: 12 }}>
+				{data?.map((item, index) => {
+					return(
+						<Grid
+							key={index}
+							item container md={2}
+							flexDirection="column"
+							onClick={() => {
+								router.push({
+									pathname:'/playlist/[slugPlaylist]',
+									query: {
+										slugPlaylist: convertSlug(String(item.title)),
+										id: item?.id,
+									}
+								})
+							}}>
+							<Grid item xs sx={{ cursor: 'pointer' }}>
+								<img alt="" src={item?.thumbnail} style={{ height: '100%', width: '100%', borderRadius: '6px' }}/>
+							</Grid>
+							<Grid item sx={{ marginY: '8px' }}>
+								<TextLineClamp
+									sx={{ fontSize: '15px', fontWeight: '600', color: 'white', textTransform: 'none'}}
+									line={1}>
+									{item?.title}
+								</TextLineClamp>
+							</Grid>
+						</Grid>
+					);
+				})}
+			</Grid>
+		</>
+	)
+}
+
+export default ListPlaylist;
