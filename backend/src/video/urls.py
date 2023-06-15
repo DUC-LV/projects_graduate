6634deda689881933e5ef6dc6dafdade17160713
DucLV08 <duc080999@gmail.com>
2023-06-15 19:17:36 +0700

    api video detail, api video recommend

--- backend/src/video/urls.py
+++ backend/src/video/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('update/topic-video', views.TopicVideoAPIView.as_view()),
     path('update/video', views.VideoAPIView.as_view()),
     path('category-video/<str:id>', views.GetCategoryVideoAPIView.as_view()),
+    path('video/<str:id>', views.GetVideoDetailAPIView.as_view()),
+    path('video-recommend/<str:id>', views.GetRecommendVideoAPIView.as_view()),
 ]
